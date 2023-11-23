402e4834791a23700b7085b0c2d36fbdec1a6f26
Chandra Srinivas P <chandrasrinivas12@gmail.com>
2023-11-23 15:31:22 +0530

    oldboy

--- frontend/src/Screens/Student/Marks.jsx
+++ frontend/src/Screens/Student/Marks.jsx
@@ -6,8 +6,8 @@ import Heading from "../../components/Heading";
 
 const Marks = () => {
   const { userData } = useSelector((state) => state);
-  const [internal, setInternal] = useState();
-  const [external, setExternal] = useState();
+  const [internal, setInternal] = useState({});
+  const [external, setExternal] = useState({});
 
   useEffect(() => {
     const headers = {
@@ -15,70 +15,74 @@ const Marks = () => {
     };
     axios
       .post(
-        https://mern-college-app.onrender.com,
+        "https://mern-college-app.onrender.com",
         { enrollmentNo: userData.enrollmentNo },
         {
           headers: headers,
         }
       )
       .then((response) => {
-        if (response.data) {
-          setInternal(response.data.Mark[0].internal);
-          setExternal(response.data.Mark[0].external);
+        if (response.data && response.data.Mark && response.data.Mark.length > 0) {
+          setInternal(response.data.Mark[0].internal || {});
+          setExternal(response.data.Mark[0].external || {});
+        } else {
+          // If no data or structure doesn't match, set internal and external as empty objects
+          setInternal({});
+          setExternal({});
         }
       })
       .catch((error) => {
         toast.dismiss();
-        toast.error(error.response.data.message);
+        toast.error(error.response?.data?.message || "Error fetching data");
+        // Set internal and external states as empty objects on error
+        setInternal({});
+        setExternal({});
       });
-  }, []);
+  }, [userData.enrollmentNo]);
 
   return (
     <div className="w-[85%] mx-auto mt-10 flex justify-center items-start flex-col mb-10">
       <Heading title={`Marks of Semester ${userData.semester}`} />
       <div className="mt-14 w-full flex gap-20">
-        {internal && (
+        {Object.keys(internal).length > 0 && (
           <div className="w-1/2 shadow-md p-4">
             <p className="border-b-2 border-red-500 text-2xl font-semibold pb-2">
               Internal Marks (Out of 40)
             </p>
             <div className="mt-5">
-              {Object.keys(internal).map((item, index) => {
-                return (
-                  <div
-                    key={index}
-                    className="flex justify-between items-center w-full text-lg mt-2"
-                  >
-                    <p className="w-full">{item}</p>
-                    <span>{internal[item]}</span>
-                  </div>
-                );
-              })}
+              {Object.keys(internal).map((item, index) => (
+                <div
+                  key={index}
+                  className="flex justify-between items-center w-full text-lg mt-2"
+                >
+                  <p className="w-full">{item}</p>
+                  <span>{internal[item]}</span>
+                </div>
+              ))}
             </div>
           </div>
         )}
-        {external && (
+        {Object.keys(external).length > 0 && (
           <div className="w-1/2 shadow-md p-4">
             <p className="border-b-2 border-red-500 text-2xl font-semibold pb-2">
               External Marks (Out of 60)
             </p>
             <div className="mt-5">
-              {Object.keys(external).map((item, index) => {
-                console.log(external);
-                return (
-                  <div
-                    key={index}
-                    className="flex justify-between items-center w-full text-lg mt-2"
-                  >
-                    <p className="w-full">{item}</p>
-                    <span>{external[item]}</span>
-                  </div>
-                );
-              })}
+              {Object.keys(external).map((item, index) => (
+                <div
+                  key={index}
+                  className="flex justify-between items-center w-full text-lg mt-2"
+                >
+                  <p className="w-full">{item}</p>
+                  <span>{external[item]}</span>
+                </div>
+              ))}
             </div>
           </div>
         )}
-        {!internal && !external && <p>No Marks Available At The Moment!</p>}
+        {Object.keys(internal).length === 0 && Object.keys(external).length === 0 && (
+          <p>No Marks Available At The Moment!</p>
+        )}
       </div>
     </div>
   );
